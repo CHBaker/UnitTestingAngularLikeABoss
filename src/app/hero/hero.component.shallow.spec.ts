4b8131cfa23011c6bee0dd377f6f6b9ae74f12ec
CHBaker <charlesprobaker@mail.com>
2018-04-17 14:34:40 -0600

    TEST: fixture test for data persistance to DOM
    - check name is rendered in a tag,
    - check name property is set

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,27 @@
+import { HeroComponent } from './hero.component';
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+describe('Hero Component (shallow)', () => {
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+        this.fixture = <ComponentFixture<HeroComponent>>TestBed.createComponent(HeroComponent)
+    })
+
+    it('should have the correct hero', () => {
+        this.fixture.componentInstance.hero = { id:1, name: 'regular guy', strength: 500 };
+
+        expect(this.fixture.componentInstance.hero.name).toEqual('regular guy');
+    })
+
+    it('should have the hero name inside an anchor tag', () => {
+        this.fixture.componentInstance.hero = { id:1, name: 'regular guy', strength: 500 };
+        this.fixture.detectChanges();
+        expect(this.fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('regular guy');
+    })
+})
