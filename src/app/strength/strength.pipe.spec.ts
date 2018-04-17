857f3dfdf502c072a7149955abe0c4fc8f1e5298
CHBaker <charlesprobaker@mail.com>
2018-04-17 11:32:33 -0600

    TESTS: pipe: strenght pipe test
    - test if strong or weak

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -8,4 +8,12 @@ describe('Pipe: strength', () => {
 
         expect(result).toBe('5 (weak)');
     })
+
+    it('should display strong if strength is 10', () => {
+        let pipe = new StrengthPipe();
+
+        let result = pipe.transform(10);
+
+        expect(result).toBe('10 (strong)');
+    })
 })
