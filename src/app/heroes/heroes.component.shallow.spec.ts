bc8a53cb6568d74a0142a607864b0b733a890e7c
CHBaker <charlesprobaker@mail.com>
2018-04-17 15:03:28 -0600

    TEST: mock hero component in shallow test

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,19 +1,48 @@
+import { of } from 'rxjs/observable/of';
+import { HeroService } from './../hero.service';
 import { HeroesComponent } from './heroes.component';
 import { TestBed, ComponentFixture } from "@angular/core/testing";
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Component, Input } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
+@Component({
+    selector: 'app-hero',
+    template: '<div></div>'
+})
+class MockHeroComponent {
+    @Input() hero;
+}
+
 describe('Heroes Component (shallow)', () => {
+    let heroes;
+    let mockHeroService: jasmine.SpyObj<HeroService>;
 
     beforeEach(() => {
+        heroes = [
+            { id:1, name: 'SpiderDude', strength: 8 },
+            { id:2, name: 'Wonderful Woman', strength: 24 },
+            { id:3, name: 'Regular Man', strength: 2 },
+        ]
+        mockHeroService = jasmine.createSpyObj(['getHeroes']);
+
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [HeroesComponent, MockHeroComponent],
+            providers: [
+                { provide: HeroService, useValue: mockHeroService}
+            ],
             schemas: [NO_ERRORS_SCHEMA]
         })
         this.fixture = <ComponentFixture<HeroesComponent>>TestBed.createComponent(HeroesComponent)
     })
 
-    it('should set heroes correctly from service', () => {
+    describe('ngOnInit()', () => {
+
+        it('should set heroes correctly from service', () => {
+            mockHeroService.getHeroes.and.returnValue(of(heroes));
+            this.fixture.detectChanges()
+
+            expect(this.fixture.componentInstance.heroes.length).toEqual(3);
+        })
 
     })
 
